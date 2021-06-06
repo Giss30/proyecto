c926e7c1a2624f6aeb07df5d6da239f3964c2e54
saul3 <saul3@LAPTOP-DGD5E4P3>
2021-06-06 16:30:00 -0500

    proyecto

--- src/main/java/com/example/tiendaonline/proyectomvc/servicios/RepoProducto.java
+++
@@ -1,9 +0,0 @@
-package com.example.tiendaonline.proyectomvc.servicios;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.tiendaonline.proyectomvc.modelo.Producto;
-
-public interface RepoProducto extends CrudRepository<Producto, Integer> {
-
-}
--- src/main/java/com/example/tiendaonline/proyectomvc/servicios/RepoUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.example.tiendaonline.proyectomvc.servicios;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.tiendaonline.proyectomvc.modelo.Usuario;
-
-public interface RepoUsuario extends CrudRepository <Usuario, Integer> {
-
-}
